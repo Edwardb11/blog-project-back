dd51d3e320109e91650ab9cb83d68f904290c0c1
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-18 12:26:32 -0400

    automatically updating with nodemon

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ app.get("/hello", (req, res) => res.send("Hello World!"));
 //post
 app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`));
 
-//
+//parameters
 app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
+
 app.listen(8000, () => console.log("Listening on port 8000"));
