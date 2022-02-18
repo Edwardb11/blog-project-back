a93a46cfd221cc3cc783282a3fcd5eaddae62d4b
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-18 12:39:10 -0400

    add comments funcionality test:postamn

--- src/server.js
+++ src/server.js
@@ -1,15 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const articlesInfo = {
+  "learn-react": {
+    comments: [],
+  },
+  "learn-node": {
+    comments: [],
+  },
+  "my-thoughts-on-learning-react": {
+    comments: [],
+  },
+};
+
+//applicacion express
 const app = express();
 
 app.use(bodyParser.json());
-//get
-app.get("/hello", (req, res) => res.send("Hello World!"));
 
-//post
-app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`));
-
-//parameters
-app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
-
+app.post("/api/articles/:name/add-comments", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+  articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName])
+});
 app.listen(8000, () => console.log("Listening on port 8000"));
+
+//Pruebas
+//get
+// app.get("/hello", (req, res) => res.send("Hello World!"));
+
+// //post
+// app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`));
+
+// //parameters
+// app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
