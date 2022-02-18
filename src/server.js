fea724569da44927f3904ae06776855168c5ac66
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-18 14:31:34 -0400

    adding MongoDB to Express

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
 
 const articlesInfo = {
   "learn-react": {
@@ -18,11 +19,28 @@ const app = express();
 
 app.use(bodyParser.json());
 
+//adding MongoDB to Express
+app.get("/api/articles/:name", async (req, res) => {
+try {
+    const articleName = req.params.name;
+    const client =await MongoClient.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+      UseUnifiedTopology: true,
+    });
+    const db = client.db("myblog");
+    const articlesInfo = await db.collection("articles").findOne({ name: articleName });
+    res.status(200).json(articlesInfo);
+    client.close();
+} catch (error) {
+    res.status(500).json({message:"error connecting to db", error})
+}
+});
+
 app.post("/api/articles/:name/add-comments", (req, res) => {
   const { username, text } = req.body;
   const articleName = req.params.name;
   articlesInfo[articleName].comments.push({ username, text });
-  res.status(200).send(articlesInfo[articleName])
+  res.status(200).send(articlesInfo[articleName]);
 });
 app.listen(8000, () => console.log("Listening on port 8000"));
 
